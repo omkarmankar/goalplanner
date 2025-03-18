4d4421cbe161468e8166270819df20dcabc2261f
Omkar Mankar <omkar.mankar2@gmail.com>
2025-03-18 15:41:20 +0530

    folder structure

---
+++ src/app/core/constant/constant.ts
@@ -0,0 +1,22 @@
+export const constant = {
+    API_METHOD_NAME:{
+        USER:{
+            GET_ALL_USERS:"GetAllUsers",
+            CREATE_USER:"Register"
+        },
+        JOB:{
+            GET_ALL_JOBS:"GetAllJobs"
+        }
+    },
+    SECURITY_KEYS:{
+
+    },
+    VALIDATION_MESSAGES:{
+        REQUIRED:"This is Required",
+        EMAIL:"Email Id is Invalid",
+    },
+    REG_EX:{
+        PANCARD:"",
+        AADHAR_CARD:""
+    }
+}
